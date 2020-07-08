a11b5dc57581a8c0e25a07795444a7f7a06b5a53
Jose Eduardo <jerp4@hotmail.com>
2020-07-08 15:23:50 -0300

    Updates

---
+++ precache-manifest.22511a0f792a12a3bea5491b566aaf8a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f0ffb53c07754e6ab81a81366e31283",
+    "url": "/front-entenda-direito/index.html"
+  },
+  {
+    "revision": "7ebfa7f35e3cac032bdc",
+    "url": "/front-entenda-direito/static/js/2.b9ee9291.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/front-entenda-direito/static/js/2.b9ee9291.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "600bc64c739dbbf82a1b",
+    "url": "/front-entenda-direito/static/js/main.b13e0720.chunk.js"
+  },
+  {
+    "revision": "d028aeea9574a901438b",
+    "url": "/front-entenda-direito/static/js/runtime-main.0cc37f45.js"
+  },
+  {
+    "revision": "a49dcf9c8ccd3029da029bfc746c5e16",
+    "url": "/front-entenda-direito/static/media/background.a49dcf9c.jpg"
+  },
+  {
+    "revision": "6c919960cea042ff6843b9dddc17aced",
+    "url": "/front-entenda-direito/static/media/forgot-password.6c919960.jpg"
+  },
+  {
+    "revision": "c65fd4bfdcabdce19a2581f7543b3101",
+    "url": "/front-entenda-direito/static/media/logo.c65fd4bf.svg"
+  },
+  {
+    "revision": "526a333a9a04b472ff8330fa9bac37cc",
+    "url": "/front-entenda-direito/static/media/sign-up.526a333a.jpg"
+  }
+]);
